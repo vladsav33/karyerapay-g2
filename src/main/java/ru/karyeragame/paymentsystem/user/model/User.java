417e7cfdb532dafa7666e55cc02d705a43f25c30
Konstantin <ya-konstant@yandex.ru>
2024-01-20 22:14:29 +0300

    Исправил import *

--- src/main/java/ru/karyeragame/paymentsystem/user/model/User.java
+++ src/main/java/ru/karyeragame/paymentsystem/user/model/User.java
@@ -1,6 +1,13 @@
 package ru.karyeragame.paymentsystem.user.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
